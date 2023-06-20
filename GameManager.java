9931061c8efdbee746e8ab929e82f43cde5e659b
DeutFusion <136686227+DeutFusion@users.noreply.github.com>
2023-06-20 14:54:04 +0200

    Add files via upload

--- GameManager.java
+++ GameManager.java
@@ -1,13 +1,4 @@
 public class GameManager
 {
-    private static GameManager Instance;
-    private Brett Brett;
-    private Spieler[] Spieler;
-    
-    GameManager()
-    {
-        Instance = this;
-        Spieler = new Spieler[4];
-        Brett = new Brett();
-    }
+    //hallo julian
 }
--- Wuerfel.java
+++ Wuerfel.java
@@ -1,5 +1,31 @@
+import java.util.Random;
+
 public class Wuerfel
 {
-    //package für zufallszahl einfuegen
-    //public int wuerfeln() hinzufuegen (zahl von 1 - 6 returnen
+    public void wuerfeln()
+    {
+        int x = (int)(Math.random() * ((6 - 1) + 1)) + 1;
+        System.out.println(x);
+    }
+    
+    public boolean startwurfeln()
+    {
+        int x = (int)(Math.random() * ((6 - 1) + 1)) + 1;
+        if(x == 6)
+        {
+            return true;
+        }
+        int y = (int)(Math.random() * ((6 - 1) + 1)) + 1;
+        if(y == 6)
+        {
+            return true;
+        }
+        int z = (int)(Math.random() * ((6 - 1) + 1)) + 1;
+        if(z == 6)
+        {
+            return true;
+        }
+        return false;
+    }
 }
+
