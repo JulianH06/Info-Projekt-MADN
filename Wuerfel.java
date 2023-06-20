f0e254f77f5f15d58236e0277d49f8009f89eaa0
DeutFusion <136686227+DeutFusion@users.noreply.github.com>
2023-06-20 14:50:14 +0200

    Add files via upload

--- Wuerfel.java
+++ Wuerfel.java
@@ -1,5 +1,35 @@
+import java.util.Random;
+
 public class Wuerfel
 {
-    //package für zufallszahl einfuegen
-    //public int wuerfeln() hinzufuegen (zahl von 1 - 6 returnen
+    public int wuerfeln()
+    {
+        int x = (int)(Math.random() * ((6 - 1) + 1)) + 1;
+        if(x == 6)
+        {
+            return x + wuerfeln();
+        }
+        return x;
+    }
+    
+    public boolean startwurfeln()
+    {
+        int x = (int)(Math.random() * ((6 - 1) + 1)) + 1;
+        if(x == 6)
+        {
+            return true;
+        }
+        int y = (int)(Math.random() * ((6 - 1) + 1)) + 1;
+        if(y == 6)
+        {
+            return true;
+        }
+        int z = (int)(Math.random() * ((6 - 1) + 1)) + 1;
+        if(z == 6)
+        {
+            return true;
+        }
+        return false;
+    }
 }
+
