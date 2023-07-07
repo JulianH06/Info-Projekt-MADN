efe37c457e106ea0afdde8923a211ecaf1119465
LukasKre <136685524+LukasKre@users.noreply.github.com>
2023-07-07 22:08:02 +0200

    Add files via upload

--- Figur.java
+++ Figur.java
@@ -1,19 +1,34 @@
 public class Figur
 {
+    private Feld startfeld;
     private Feld position;
-    public Figur(Feld f)
+    private farbenum farbe;
+    public Figur(farbenum f)
+    {
+        startfeld = f;
+        position = f;
+    }
+    
+    public void positionSetzen(Feld f)
     {
         position = f;
     }
     
-    public Feld ziehen(int i)
+    public void reset()
     {
-        position = position.nf(i); 
-        return position;
-        
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          // haliow ist super mega duper wann gibt mir julian meine schulden wieder man fragt es sich häufig ich frage es mich täglich nach dem aufstehen und vor dem schlafen gehen und allgemein hoffe ich immer darauf aber leider wird diese hoffnung jeden tag wieder zerschlagen wenn ich ihn in der schule fragt wann er sie mir gibt und er jedes mal sagt das es nicht mehr viel ist und dass er sie mir scho noch früh genug wieder geben wird aber ich denke kaum das dies der Wahrheit entspricht da er auch in letzter Zeit kaum einen Fortschritt gelangen konnte. eine sehr trauriges Brot ist dies alles. jajaja. Monte dein Aquarium brennt. ok das wars m it meinem diktat ich habe dies alles Blind geschrieben ich hoffe es sind nicht zu viele Fehler drinne. Ich binb blind schon schneller am schreiben im Gegensatzt zu Lukas was ich ziemlich lustig finde lol ja hallo Ljukas na du alter kollege warum kuckst du so komisch hallo na du julian ist ein toller game master er steuert und hilft uns allen gerne er trägt heute ein braunes tshirt mit jack % jones drauf geschrieben mit einer dunkel blauen jeans und fabian hat auf seinem tshirt den namen "hilfiger" stehen lol fortnite  
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+        position = startfeld;
     }
+    
+    // public Feld ziehen(int i)
+    // {
+        // position.
+        
+        // //position = position.nf(i); 
+        // //return position;
+        
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          // // haliow ist super mega duper wann gibt mir julian meine schulden wieder man fragt es sich häufig ich frage es mich täglich nach dem aufstehen und vor dem schlafen gehen und allgemein hoffe ich immer darauf aber leider wird diese hoffnung jeden tag wieder zerschlagen wenn ich ihn in der schule fragt wann er sie mir gibt und er jedes mal sagt das es nicht mehr viel ist und dass er sie mir scho noch früh genug wieder geben wird aber ich denke kaum das dies der Wahrheit entspricht da er auch in letzter Zeit kaum einen Fortschritt gelangen konnte. eine sehr trauriges Brot ist dies alles. jajaja. Monte dein Aquarium brennt. ok das wars m it meinem diktat ich habe dies alles Blind geschrieben ich hoffe es sind nicht zu viele Fehler drinne. Ich binb blind schon schneller am schreiben im Gegensatzt zu Lukas was ich ziemlich lustig finde lol ja hallo Ljukas na du alter kollege warum kuckst du so komisch hallo na du julian ist ein toller game master er steuert und hilft uns allen gerne er trägt heute ein braunes tshirt mit jack % jones drauf geschrieben mit einer dunkel blauen jeans und fabian hat auf seinem tshirt den namen "hilfiger" stehen lol fortnite  
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    // }
     
     
 }
