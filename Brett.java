559aee34c9bafdc27acf803bbcec6decc11d6937
Buthur <136681984+Buthur@users.noreply.github.com>
2023-07-11 15:09:38 +0200

    Add files via upload

--- Brett.java
+++ Brett.java
@@ -1,10 +1,29 @@
+import java.util.Arrays;
 
 public class Brett
 {
     private Feld[] Felder;
     private int laenge = 40;
     
-  
+    
+    private Feld[] BlauStart;
+    
+    private Feld[] GruenStart;
+    
+    private Feld[] RotStart;
+    
+    private Feld[] GelbStart;
+    
+    
+    private Feld[] BlauEnde;
+    
+    private Feld[] GrünEnde;
+    
+    private Feld[] RotEnde;
+    
+    private Feld[] GelbEnde;
+    
+    
     public void Brett()
     {
         Felder= new Feld[40];
